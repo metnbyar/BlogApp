58b725ed1d9a5dbfbcdc1c234a46b19748b90072
metnbyar <99257085+metnbyar@users.noreply.github.com>
2025-06-13 18:43:52 +0300

    Login-Logout işlemleri

--- BlogApp.Web/Areas/Admin/Controllers/HomeController.cs
+++ BlogApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using BlogApp.Service.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IArticleService articleService;
